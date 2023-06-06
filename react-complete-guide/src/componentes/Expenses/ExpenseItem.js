6128275fd7a7011cd18322ba57f3af5560a4cb50
gabrieldal <88405111+gabrieldal@users.noreply.github.com>
2023-06-06 17:52:27 -0300

    adicionado lógica para dar update nos botoês com useState

--- react-complete-guide/src/componentes/Expenses/ExpenseItem.js
+++ react-complete-guide/src/componentes/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
@@ -6,15 +7,17 @@ import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
 
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log('Clicked!!!');
+    setTitle('Updated!');
   };
   
   return (
     <Card className="expense-item">
      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
       </div>
       <div className="expense-item__price">{props.amount}</div>
       <button onClick= {clickHandler}>Change Title</button>

d7cc991a2c66ff454590b0048c8c9a21f6c27954
gabrieldal <88405111+gabrieldal@users.noreply.github.com>
2023-06-08 10:47:34 -0300

    add forms logic

--- react-complete-guide/src/componentes/Expenses/ExpenseItem.js
+++ react-complete-guide/src/componentes/Expenses/ExpenseItem.js
@@ -8,9 +8,11 @@ import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
 
   const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
 
   const clickHandler = () => {
     setTitle('Updated!');
+    console.log(title);
   };
   
   return (

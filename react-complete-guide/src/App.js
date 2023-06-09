51a004231b02b71aaba9acf42361c9467894b64a
gabrieldal <88405111+gabrieldal@users.noreply.github.com>
2023-06-09 15:24:06 -0300

    add ID logic

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,40 +1,43 @@
+import React, { useState } from "react";
 import "./App.css";
 import Expenses from "./componentes/Expenses/Expenses";
 import NewExpense from "./componentes/NewExpense/NewExpense";
 
+const Dummy_Expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(Dummy_Expenses);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]; // This is the best way to update the state when it depends on the previous state.
+    });
   };
 
   return (
     <div>
-        <NewExpense onAddExpense={addExpenseHandler}/>
-        <Expenses itens={expenses}/>
-      
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses itens={expenses} />
     </div>
   );
 }
--- react-complete-guide/src/componentes/Expenses/ExpenseItem.js
+++ react-complete-guide/src/componentes/Expenses/ExpenseItem.js
@@ -1,28 +1,17 @@
-import React, {useState} from "react";
-import "./ExpenseItem.css";
-import Card from "../UI/Card";
+import React from 'react';
 
-import ExpenseDate from "./ExpenseDate";
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-
-function ExpenseItem(props) {
-
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
-
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  };
-  
+const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-     <ExpenseDate date={props.date}/>
-      <div className="expense-item__description">
-        <h1>{title}</h1>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <div className="expense-item__price">{props.amount}</div>
-      <button onClick= {clickHandler}>Change Title</button>
     </Card>
   );
 }

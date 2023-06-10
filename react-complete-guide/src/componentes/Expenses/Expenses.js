2ecb19f9cb67da9a7606046e6344591090f83bfc
gabrieldal <88405111+gabrieldal@users.noreply.github.com>
2023-06-10 15:56:34 -0300

    projeto finalizado em aula

--- react-complete-guide/src/componentes/Expenses/Expenses.js
+++ react-complete-guide/src/componentes/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -24,6 +25,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
       </list>
